cb2dce799bdd72bb89f7e322d11d029aa64f8bd4
Connor Christensen <connor.christensen@uvu.edu>
2023-02-03 16:47:44 -0700

    Wrote Subtraction Test

--- jestPractice/tests/calculator.test.js
+++ jestPractice/tests/calculator.test.js
@@ -10,3 +10,14 @@ test('addition calculator', () => {
   expect(sum(7, 44)).toBe(51);
 });
 
+
+
+
+
+// TEST
+test('subtraction calculator', () => {
+  expect(sum(32, 1)).toBe(31);
+});
+test('subtraction calculator', () => {
+  expect(sum(15, 20)).toBe(-5);
+});
