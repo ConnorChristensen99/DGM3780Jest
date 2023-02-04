5c8df0cd2f1543be999458565d8653b9e7547630
Connor Christensen <connor.christensen@uvu.edu>
2023-02-03 17:33:02 -0700

    Wrote function for object key pairings

--- jestPractice/tests/objects.test.js
+++ jestPractice/tests/objects.test.js
@@ -1,7 +1,12 @@
+//Function for the object key pairings
+function objectTest(obj,key) {
+    return key in obj
+}
+
 // TEST
 test('object key pairings', () => {
-    expect(objectTest({name: "John Doe", age: 16}),'name').toBe(true);
+    expect(objectTest({name: "John Doe", age: 16},'name')).toBe(true);
   });
   test('object key pairings', () => {
-    expect(objectTest({name: "John Doe", age: 16}),'occupation').toBe(false);
+    expect(objectTest({name: "John Doe", age: 16},'occupation')).toBe(false);
   });
