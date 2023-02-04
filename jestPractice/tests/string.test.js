ab0e24aa4ec05f58417866ae9d4761e254d86fc3
Connor Christensen <connor.christensen@uvu.edu>
2023-02-03 17:02:33 -0700

    Wrote test for reverse string

---
+++ jestPractice/tests/string.test.js
@@ -0,0 +1,13 @@
+
+
+
+// TEST
+test('reverse string', () => {
+    expect(reverse("Jonas")).toBe("sanoJ");
+  });
+  test('reverse string', () => {
+    expect(reverse("sample")).toBe("elpmas");
+  });
+  test('reverse string', () => {
+    expect(reverse("SpOnGeBoB")).toBe("BoBeGnOpS");
+  });
