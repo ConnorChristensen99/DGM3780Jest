e7eccf08aefd31b42b3e1c2052a637889c6ffb01
Connor Christensen <connor.christensen@uvu.edu>
2023-02-07 16:35:57 -0700

    Wrote function for typeof test

--- jestPractice/tests/type.test.js
+++ jestPractice/tests/type.test.js
@@ -1,22 +1,33 @@
+
+//Functions for type of
+function myFunction(type) {
+    return type
+
+}
+
+
+
 // TEST
-test('null test', () => {
-    expect(typeof myFunction(typeof null)).toBe("null");
-  });
   test('boolean test', () => {
-    expect(typeof myFunction(typeof true)).toBe("boolean");
+    expect(myFunction(typeof true)).toBe("boolean");
   });
-  test('unidentified test', () => {
-    expect(typeof myFunction(typeof unidentified)).toBe("unidentified");
+  test('undefined test', () => {
+    expect(myFunction(typeof undefined)).toBe("undefined");
   });
   test('number test', () => {
-    expect(typeof myFunction(typeof 43)).toBe("number");
+    expect(myFunction(typeof 43)).toBe('number');
   });
   test('bigint test', () => {
-    expect(typeof myFunction(typeof 9007199254740991)).toBe("bigint");
+    expect(myFunction(typeof BigInt(9007199254740991))).toBe("bigint");
   });
   test('string test', () => {
-    expect(typeof myFunction(typeof "Hello")).toBe("string");
+    expect(myFunction(typeof "Hello")).toBe('string');
   });
   test('symbol test', () => {
-    expect(typeof myFunction(typeof $)).toBe("symbol");
+    sym=Symbol('foo')
+    expect(myFunction(typeof sym)).toBe('symbol');
   })
+  test('null test', () => {
+    expect(myFunction(typeof null)).toBe("object");
+  });
+
